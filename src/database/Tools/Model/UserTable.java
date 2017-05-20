78751039d6edf43ecad68881c628026e1bd320f3
jordicala <jordicatasuslajas@gmail.com>
2017-05-20 16:41:08 +0200

    UserTable created

--- src/database/Tools/Model/UserTable.java
+++ src/database/Tools/Model/UserTable.java
@@ -2,7 +2,7 @@ package database.Tools.Model;
 
 import java.sql.Timestamp;
 
-public class UserTable {
+public class UserTable extends Table{
 
 	private String id;
 	private String totalPoints;
@@ -20,12 +20,13 @@ public class UserTable {
 	private String description;
 	private String registerDate;
 	private String lastConnectionDate;
+        private String isActive;
 	private String sql;
 	private String table;
 	
 	
 	public UserTable() {
-	
+                this.isActive = "isActive";
 		this.table = "user"; 
 		this.id = "id";
 		this.totalPoints = "totalPoints";
@@ -45,103 +46,6 @@ public class UserTable {
 		this.lastConnectionDate = "lastConnectionDate";
 	}
 	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getTotalPoints() {
-		return totalPoints;
-	}
-	public void setTotalPoints(String totalPoints) {
-		this.totalPoints = totalPoints;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getBattleName() {
-		return battleName;
-	}
-	public void setBattleName(String battleName) {
-		this.battleName = battleName;
-	}
-	public String getBattleTag() {
-		return battleTag;
-	}
-	public void setBattleTag(String battleTag) {
-		this.battleTag = battleTag;
-	}
-	public String getLogo() {
-		return logo;
-	}
-	public void setLogo(String logo) {
-		this.logo = logo;
-	}
-	public String getFinalStatisticsId() {
-		return finalStatisticsId;
-	}
-	public void setFinalStatisticsId(String finalStatisticsId) {
-		this.finalStatisticsId = finalStatisticsId;
-	}
-	public String getGradeId() {
-		return gradeId;
-	}
-	public void setGradeId(String gradeId) {
-		this.gradeId = gradeId;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getTwitter() {
-		return twitter;
-	}
-	public void setTwitter(String twitter) {
-		this.twitter = twitter;
-	}
-	public String getTwitch() {
-		return twitch;
-	}
-	public void setTwitch(String twitch) {
-		this.twitch = twitch;
-	}
-	public String getRightsId() {
-		return rightsId;
-	}
-	public void setRightsId(String rightsId) {
-		this.rightsId = rightsId;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getRegisterDate() {
-		return registerDate;
-	}
-	public void setRegisterDate(String registerDate) {
-		this.registerDate = registerDate;
-	}
-	public String getLastConnectionDate() {
-		return lastConnectionDate;
-	}
-	public void setLastConnectionDate(String lastConnectionDate) {
-		this.lastConnectionDate = lastConnectionDate;
-	}
-	
 	public String generateSQLString(){
 		
 //		sql = "CREATE TABLE "+this.table+" ("
@@ -151,7 +55,27 @@ public class UserTable {
 //				+ "CREATED_DATE DATE NOT NULL, " + "PRIMARY KEY (USER_ID) "
 //				+ ")";
 //		
-		sql = "CREATE TABLE Persons (  PersonID int)";
+		sql = "CREATE TABLE `user` (\n"
+                    + "`"+this.id+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.totalPoints+"` INTEGER(20) NOT NULL,\n"
+                    + "`"+this.name+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.battleName+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.battleTag+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.logo+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.finalStatisticsId+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.gradeId+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.email+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.password+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.twitter+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.twitch+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.rightsId+"` VARCHAR(20) NOT NULL,\n"
+                    + "`"+this.description+"` VARCHAR(140) NOT NULL,\n"
+                    + "`"+this.registerDate+"` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\n"
+                    + "`"+this.lastConnectionDate+"` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\n"
+                    + "`"+this.isActive+"` BOOLEAN NOT NULL DEFAULT '1',\n"
+                    + "PRIMARY KEY (`"+this.id+"`)\n"
+                    //+ "FOREIGN KEY (`"+this.gradeId+"`) REFERENCES grade(`id`)\n"
+                    + ") ENGINE=InnoDB;";
 		return sql;
 	}
 }
